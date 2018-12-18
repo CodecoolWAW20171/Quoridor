ee5e74a1ea66b195beccd5dcfa115864d7322b09
Arkadiusz Szreiber <aszreiber1891@gmail.com>
2018-12-18 11:30:45 +0100

    main bsic structure

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def route_index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
